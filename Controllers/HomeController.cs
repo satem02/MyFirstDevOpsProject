4c1e589601c15531b4ff3d039acc81be0e2de340
Şafak <safaktemel02@gmail.com>
2019-01-17 14:50:40 +0300

    Update HomeController.cs
    test

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -17,14 +17,14 @@ namespace MyFirstDevOpsProject.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "My application description page.";
 
             return View();
         }
 
         public IActionResult Contact()
         {
-            ViewData["Message"] = "Your contact page.";
+            ViewData["Message"] = "My contact page.";
 
             return View();
         }
